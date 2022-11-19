5e1d09480ab3ef51311f7f35a165268e79e7ab3e
Sahak-S <sahak.sa@mail.ru>
2022-11-19 23:52:29 +0400

    QR Code

---
+++ src/main/java/com/example/qrcodejavaspringboot/QrCodeJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.qrcodejavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeJavaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrCodeJavaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qrcodejavaspringboot/QrCodeJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qrcodejavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrCodeJavaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
